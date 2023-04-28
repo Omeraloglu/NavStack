4443f8ebacc6ab59e0bb7114a59841a983a499a5
Omer <omeraloglu16@hotmail.com>
2023-04-28 22:57:34 +0300

    Initial Commit

---
+++ NavStack/NavStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackApp.swift
+//  NavStack
+//
+//  Created by Ömer ALOĞLU on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
